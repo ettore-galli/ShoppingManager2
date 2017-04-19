556ea50d93a81cf4314c8da0d8634a089e23e1f5
Ettore Galli <ettore.galli@gmail.com>
2017-04-19 21:49:19 +0200

    Standard work in progress

--- app/src/main/java/com/example/ettoregalli/shoppingmanager2/database/sql/SQLCudQueryBuilder.java
+++ app/src/main/java/com/example/ettoregalli/shoppingmanager2/database/sql/SQLCudQueryBuilder.java
@@ -1,18 +1,75 @@
 package com.example.ettoregalli.shoppingmanager2.database.sql;
 
-import android.icu.math.BigDecimal;
-
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 /**
- * The SQLCudQueryBuilder is a utility class that enables easy writing of SQL data modification statements
- * (i.e. the CRUD operations less the "R")
+ * <p><b>Purpose and scope
+ * </p></b>
+ * <p>The SQLCudQueryBuilder is a utility class that makes writing of SQL data modification statements easy;
+ * with data modification meaning the usual CRUD operations less the "R"
+ * </p>
+ * <p><b>Definition of terms:
+ * </p></b>
+ * <ul>
+ * <li><b>CUD:</b> <i>Create, Update, Delete database operations (i.e. INSERT INTO, UPDATE, DELETE SQL statements)</i>
+ * </li>
+ * <li><b>table:</b> <i>A single database table</i>
+ * </li>
+ * <li><b>field:</b> <i>A table column together with its own value and the indication whether it is a "key" field or not.
+ * Fields can be divided into two categories according to their "key" nature:
+ * </i>
+ * <ul>
+ * <li><b>key fields:</b> <i>Fields that are used to select records in update and delete operations and that appear in the WHERE clause of SQL statements</i></li>
+ * <li><b>key fields:</b> <i>Fields whose values will be changed in UPDATE statements and that do not appear in the WHERE clause</i></li>
+ * </ul>
+ * <i>Please note that the "key" nature of fields is not related to the table primary key; when fields are added (see example below) the key nature simply represents the
+ * fields that will be used to select the records to update or delete</i>
+ * </li>
+ * </ul>
+ * <p>
+ * <p><b>How the class works and should be used:
+ * </b></p>
+ * <p>The class basically consists of a collection of fields and a table name, from which SQL statements can be constructed.
+ * The class is intended to be used as follows.
+ * </p>
+ * <p>First, create a new SQLCudQueryBuilder object:
+ * </p>
+ * {@code
+ * SQLCudQueryBuilder myBuilder = new SQLCudQueryBuilder("myTable");
+ * }
+ * <p>Then, add the fields:
+ * </p>
+ * <pre>{@code
+ * myBuilder.put("a_key_column", 123456, true);
+ * myBuilder.put("a_string_column", "value");
+ * myBuilder.put("a_numeric_column", 3.141);
+ * }
+ * </pre>
+ * <p>At this point, SQL statements are generated automatically:
+ * </p>
+ * <pre>{@code
+ * System.out.println(myBuilder.getInsertQuery());
+ * System.out.println(myBuilder.getUpdateQuery());
+ * System.out.println(myBuilder.getDeleteQuery());
+ * }
+ * </pre>
+ * <p> The result is the following:
+ * </p>
+ * <pre>{@code
+ * INSERT INTO myTable(a_numeric_column, a_string_column, a_key_column) VALUES(3.141, "value", 123456);
+ * UPDATE myTable SET a_numeric_column=3.141, a_string_column="value" WHERE (a_key_column=123456);
+ * DELETE FROM myTable WHERE (a_key_column=123456);
+ * }
+ * </pre>
  *
  * @author Ettore Galli
  * @version Pre Alpha 1
+ * @todo How the class works
+ * @todo Why "CUD"
  */
 
 public class SQLCudQueryBuilder {
@@ -35,6 +92,7 @@ public class SQLCudQueryBuilder {
     private static final String WHERE_CLAUSE_AND_SEPARATOR = " AND ";
     private static final String SQL_QUOTE = "\"";
     private static final String SQL_NULL = "NULL";
+
 
     /* The table addressed by the SQL statements */
     private String tableName;
@@ -66,6 +124,7 @@ public class SQLCudQueryBuilder {
         supportedClassList.add(BigInteger.class);
         supportedClassList.add(BigDecimal.class);
         supportedClassList.add(Boolean.class);
+        supportedClassList.add(Character.class); // Aggiunta 19/04/2017
 
     }
 
@@ -73,7 +132,7 @@ public class SQLCudQueryBuilder {
      * Adds a table field toghether with its own value
      *
      * @param fieldName      The name of the field being added
-     * @param fieldValue     The value of the field; it must be a class (one that is supported) or null
+     * @param fieldValue     The value of the field; it must be the instance of a class or null, not a primitive type.
      * @param isCrudKeyField Tells the Query Builder that the field is a key field for this specific operation; key fields are in the where clause of update and delete statements and their values are used only in insert statements.
      * @throws ClassNotSupportedException
      */
@@ -219,7 +278,7 @@ public class SQLCudQueryBuilder {
      * @param expressionSeparator     The separator being used (e.g. you can specify " AND " for where clauses)
      *                                Please note that is the caller's responsibility to insert proper spaces before and/or after the separator
      * @param expressionInParentheses If true puts the resulting expression in parentheses
-     * @return
+     * @return The expression list required
      */
     private String getFieldExpressionList(String keyValues, String expressionType, String expressionSeparator, boolean expressionInParentheses) {
 
@@ -270,22 +329,52 @@ public class SQLCudQueryBuilder {
      */
     @Override
     public String toString() {
-
+        final String key_fmt = "%-25s";
+        final String val_fmt = "%-60s";
+        final String type_fmt = "%-25s";
+        final String sep = "|";
+        String keyv;
+        String valv;
+        String typev;
         String s = "";
+        keyv = String.format(key_fmt, "Field");
+        valv = String.format(val_fmt, "Value");
+        typev = String.format(type_fmt, "Class");
+        s += keyv + sep + valv + sep + typev + "\n";
         for (String k : fieldValues.keySet()) {
+            keyv = k.trim();
             Object val = fieldValues.get(k);
-            String dspv;
-            String typev;
             if (val != null) {
-                dspv = val.toString();
+                valv = val.toString();
                 typev = val.getClass().toString();
             } else {
-                dspv = SQL_NULL;
+                valv = SQL_NULL;
                 typev = "null";
             }
-            s += k + ": \t" + dspv + "(" + typev + ")\n";
+            keyv = String.format(key_fmt, keyv);
+            valv = String.format(val_fmt, valv);
+            typev = String.format(type_fmt, typev);
+            s += keyv + sep + valv + sep + typev + "\n";
         }
         return s;
+    }
+
+    /**
+     * Gets the name of the table
+     *
+     * @return The table name
+     */
+    public String getTableName() {
+        return tableName;
+    }
+
+    /**
+     * Sets the name of the table
+     *
+     * @param tableName The table name
+     */
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
     }
 
     /* Exception thrown when an unsupported class is used for a field value */
@@ -294,5 +383,6 @@ public class SQLCudQueryBuilder {
             super(message);
         }
     }
+
 
 }
